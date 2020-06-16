4e7c33ba9a90471ee2614e3aaeb37baf80d20e4d
GHkmmm <hgmkk2299@icloud.com>
2020-06-16 23:25:28 +0800

    优化代码

---
+++ src/views/usermanage/childComps/FilterUser.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h3>搜索</h3>
+    <div>
+      <div class="d-flex">
+        <input class="form-control query-input" placeholder="请输入用户名" v-model="username">
+        <button class="btn btn-primary" @click="QueryUser">搜索</button>
+      </div>
+      <div class="switch-group">
+        <or-switch @on="onlyManager" @off="getUser"/>
+        <label class="form-check-label">
+          只查看管理员用户
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import OrSwitch from 'components/common/switch/Switch';
+
+export default {
+  name: "FilterUser",
+  data(){
+    return {
+      username: ""
+    }
+  },
+  components: {
+    OrSwitch
+  },
+  methods: {
+    onlyManager(){
+      this.$emit("onlyManager")
+    },
+    getUser(){
+      this.$emit("getUser")
+    },
+    QueryUser(){
+      this.$emit("QueryUser", this.username)
+    }
+  }
+}
+</script>
+
+<style>
+.query-input{
+  width: 200px;
+  margin-right: 10px;
+}
+.switch-group{
+  display: flex;
+  margin: 10px;
+}
+</style>
---
+++ src/views/usermanage/childComps/UserTable.vue
@@ -0,0 +1,65 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">用户名</th>
+        <th scope="col">手机号</th>
+        <th scope="col">职位</th>
+        <th scope="col">邮箱</th>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.key" class="my-tr">
+        <td>{{user.userId}}</td>
+        <td>{{user.username}}</td>
+        <td>{{user.phoneNum}}</td>
+        <td>{{user.posId|userPos}}</td>
+        <td>{{user.email}}</td>
+        <td><a href="#" @click="editInfo(user)">编辑</a></td>
+        <td><a href="#" @click="deleteUser(user.userId)">删除</a></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "UserTable",
+  props: {
+    users:{
+      type: Array,
+      default: []
+    }
+  },
+  filters: {
+    userPos(id){
+      switch(id){
+        case 1:
+          return "超级用户";
+          break;
+        case 2:
+          return "体育馆管理专员";
+          break;
+        case 3:
+          return "普通用户";
+          break;
+      }
+    }
+  },
+  methods: {
+    editInfo(user){
+      this.$emit("editInfo", user)
+    },
+    deleteUser(userId){
+      this.$emit("deleteUser", userId)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
