2919a4e50f1476448ac878920894e525cd9c5790
Sagittarius-sigma <908306869@qq.com>
2020-06-17 17:56:39 +0800

    添加裁判公告
    添加裁判公告

--- src/views/racemanage/RaceManage.vue
+++ src/views/racemanage/RaceManage.vue
@@ -1,6 +1,63 @@
 <template>
-  <div>
+  <div class="user-manage">
     
+    <!--<nav class="navbar navbar-expand-lg navbar-light bg-blue">
+  <span class="navbar-brand" >赛事管理</span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+
+      <li class="nav-item dropdown ">
+        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span>交易类型:</span>
+          {{searchTrdaingType|typeName}}
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#" @click="allType()">所有</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#" @click="onlyPay()">支出</a>
+          <a class="dropdown-item" href="#" @click="onlyRevenue()">收入</a>
+        </div>
+      </li>
+
+      <li class="nav-item dropdown ">
+        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          起始时间
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">所有</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#" >支出</a>
+          <a class="dropdown-item" href="#" >收入</a>
+        </div>
+      </li>
+
+      <li class="nav-item dropdown ">
+        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          结束时间
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">所有</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">支出</a>
+          <a class="dropdown-item" href="#">收入</a>
+        </div>
+      </li>
+
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="输入交易ID" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">搜索</button>
+    </form>
+  </div>
+</nav>-->
+
+    <bulletin ref="childbulletin"></bulletin>
+    <div>
     <table class="table">
       <thead>
         <tr>
@@ -27,16 +84,19 @@
         </tr>
       </tbody>
     </table>
-    
+    </div>
   </div>
 </template>
 
 <script>
 
 import { getGame, addGame, deleteGame } from 'network/gameManage';
-
+import bulletin from "components/content/bulletin/Bulletin";
 export default {
   name: "GameManage",
+   components: {
+    bulletin
+  },
   data(){
     return{
       games: []
@@ -44,12 +104,28 @@ export default {
     }
   },
 
+  mounted() {
+    this.changeChild();
+  },
+
   created(){
       this.getGame();
     },
  
   methods: {
-    
+    changeChild() {
+      this.$refs.childbulletin.textArr = [
+        { title: "裁判公告" },
+        { title: "裁判对待比赛需公平公正，若发现违反请及时举报" },
+        { title: "裁判长：小舔" },
+        { title: "1号裁判：阿给" },
+        { title: "2号裁判：元谋人" },
+        { title: "3号裁判：小阿giao" }
+        
+      ]; // 滚动时间
+      this.$refs.childbulletin.rotateTime = 2000;
+    },
+
     getGame(){
       getGame().then(res => {
         console.log(res);
