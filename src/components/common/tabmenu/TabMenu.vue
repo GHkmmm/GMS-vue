0909a89f3449cfefcf31544da3091c985bc405bd
GHkmmm <hgmkk2299@icloud.com>
2020-06-18 10:47:02 +0800

    美化了侧边导航栏

--- src/components/common/tabmenu/TabMenu.vue
+++ src/components/common/tabmenu/TabMenu.vue
@@ -1,23 +1,45 @@
 <template>
   <div class="tab-menu">
-    <div class="menu-list">
-      <div v-for="(menu,index) in menus" 
-          :key="index" 
-          class="menu-list-item" 
-          :class="{actived: currentIndex==index}"
-          @click="itemClick(index)"
-          v-show="menu.name">
-          <img :src="menu.icon|getImg" alt="">
-          <div>{{menu.name}}</div>
-          <!-- <item-menu :menuitem="menu.menu" :isShowItem="currentIndex==index" /> -->
+    <div class="menu-list"
+          @click="showItemMenu1"
+          :class="{actived: isShowItemMenu1}">
+          <img class="menu-list-item-title-icon" src="~assets/img/tabmenu/home.svg" alt="">
+          <div>首页</div>
+          <img class="menu-down" src="~assets/img/tabmenu/down.svg" alt="">
+    </div>
+    <div class="menu-list-item-container" :class="{menuListItemContainerActived: isShowItemMenu1}">
+      <div v-for="(menu,index) in home" 
+        :key="index" 
+        class="menu-list-item" 
+        :class="{actived: homeCurrentIndex==index}"
+        @click="HomeItemClick(index,menu.path)"
+        v-show="menu.name">
+        <img :src="menu.icon|getImg" alt="">
+        <div>{{menu.name}}</div>
+      </div>
+    </div>
+    <div class="menu-list"
+          @click="showItemMenu2"
+          :class="{menuListActived: isShowItemMenu2}">
+          <img class="menu-list-item-title-icon" src="~assets/img/tabmenu/dashboard.svg" alt="">
+          <div>控制台</div>
+          <img class="menu-down" src="~assets/img/tabmenu/down.svg" alt="">
+    </div>
+    <div class="menu-list-item-container" :class="{menuListItemContainerActived: isShowItemMenu2}">
+      <div v-for="(menu,index) in dashboard" 
+        :key="index" 
+        class="menu-list-item" 
+        :class="{actived: dashboardCurrentIndex==index}"
+        @click="DashboardItemClick(index,menu.path)"
+        v-show="menu.name">
+        <img :src="menu.icon|getImg" alt="">
+        <div>{{menu.name}}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import itemMenu from './itemMenu';
-
 export default {
   name: "TabMenu",
   props: {
@@ -28,12 +50,14 @@ export default {
       }
     }
   },
-  components: {
-    itemMenu
-  },
   data(){
     return {
-      currentIndex: 0
+      homeCurrentIndex: 0,
+      dashboardCurrentIndex: 0,
+      isShowItemMenu1: true,
+      isShowItemMenu2: false,
+      home:[],
+      dashboard: []
     }
   },
   filters: {
@@ -43,14 +67,47 @@ export default {
       }
     }
   },
+  created(){
+    this.SplitArray();
+  },
   methods: {
-    itemClick(index){
-      if(this.currentIndex != index){
-        this.currentIndex = index;
-        this.$emit('menuItemClick', this.menus[index].path)
+    HomeItemClick(index,path){
+      if(this.homeCurrentIndex != index){
+        this.homeCurrentIndex = index;
+        this.$emit('menuItemClick', path)
       }
     },
-    
+    DashboardItemClick(index,path){
+      if(this.dashboardCurrentIndex != index){
+        this.dashboardCurrentIndex = index;
+        this.$emit('menuItemClick', path)
+      }
+    },
+    showItemMenu1(){
+      this.isShowItemMenu1 = !this.isShowItemMenu1
+      this.isShowItemMenu2 = false;
+      if(isShowItemMenu1==true){
+        this.$emit('menuItemClick', "rentEquipment")
+      }
+    },
+    showItemMenu2(){
+      this.isShowItemMenu1 = false;
+      this.isShowItemMenu2 = !this.isShowItemMenu2
+      if(isShowItemMenu2==true){
+        this.$emit('menuItemClick', "user")
+      }
+    },
+    SplitArray(){
+      this.menus.forEach((item, index) => {
+        if(index<=3){
+          this.home.push(item)
+        }else{
+          this.dashboard.push(item)
+        }
+      })
+      console.log(this.home);
+      console.log(this.dashboard);
+    }
   }
 }
 </script>
@@ -59,24 +116,39 @@ export default {
 .tab-menu{
   height: 100%;
   width: 20%;
-  text-align: center;
-  background-color: #eee;
+  background-color: #fff;
   color: #666;
   font-size: 14px;
+  overflow-y: scroll;
 }
-.menu-list-item{
+.menu-list, .menu-list-item{
   line-height: 80px;
   cursor: pointer;
   box-sizing: border-box;
   display: flex;
-  justify-content: center;
+  padding-left: 20px;
+  position: relative;
 }
-.menu-list-item img{
-  width: 25px;
-  margin-right: 5px;
+.menu-list img, .menu-list-item img{
+  width: 20px;
+  margin-right: 25px;
 }
 .actived{
-  background-color: #fff;
-  border-left: 4px solid #047AFB
+  background-color: #eee;
+}
+.menu-list-item-container{
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height .4s;
+}
+.menuListItemContainerActived{
+  max-height: 300px;
+}
+.menu-down{
+  width: 10px;
+  height: 10px;
+  position: absolute;
+  right: 10px;
+  top: 45%;
 }
 </style>
